290aaf9dfb8fc670c2f758622a468e01219b9044
edrazi90 <edrazi90@gmail.com>
2021-01-23 00:52:17 +0800

    Added Firebase Authentication and Forgot password feature

---
+++ Rayie_Quizz/lib/Register.dart
@@ -0,0 +1,192 @@
+import 'package:QuizzTime/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'home.dart';
+
+
+class RegisterPage extends StatefulWidget {
+  RegisterPage({Key key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final GlobalKey<FormState> _registerFormKey = GlobalKey<FormState>();
+  TextEditingController firstNameInputController;
+  TextEditingController lastNameInputController;
+  TextEditingController emailInputController;
+  TextEditingController pwdInputController;
+  TextEditingController confirmPwdInputController;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference collectionReference; 
+  final Query users = FirebaseFirestore.instance.collectionGroup("users");
+ 
+  @override
+  initState() {
+    firstNameInputController = new TextEditingController();
+    lastNameInputController = new TextEditingController();
+    emailInputController = new TextEditingController();
+    pwdInputController = new TextEditingController();
+    confirmPwdInputController = new TextEditingController();
+    super.initState();
+  }
+
+  
+  String emailValidator(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'Email format is invalid';
+    } else {
+      return null;
+    }
+  }
+
+  String pwdValidator(String value) {
+    if (value.length < 8) {
+      return 'Password must be longer than 8 characters';
+    } else {
+      return null;
+    }
+  }
+
+  String nameValidator(String value){
+    if(value.length<3){
+      return'Minimum 4 characters!';
+    }else{
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Register"),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+                child: Form(
+              key: _registerFormKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'First Name:'),
+                    controller: firstNameInputController,
+                    validator: nameValidator,
+                  ),
+                  TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Last Name:'),
+                      controller: lastNameInputController,
+                      validator: nameValidator
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Email:'),
+                    controller: emailInputController,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: emailValidator,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Password:'),
+                    controller: pwdInputController,
+                    obscureText: true,
+                    validator: pwdValidator,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Confirm Password:'),
+                    controller: confirmPwdInputController,
+                    obscureText: true,
+                    validator: pwdValidator,
+                  ),
+                  MaterialButton(
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    hoverColor: Colors.white,
+                    elevation: 9.0,
+                     minWidth: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                      child: Text(
+                        "Register",
+                        style: style.copyWith(color: Colors.white,fontWeight: FontWeight.bold),
+                      ),
+                    onPressed: () {
+                      if (_registerFormKey.currentState.validate()) {
+                        if (pwdInputController.text ==
+                            confirmPwdInputController.text) {
+                          FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                                  email: emailInputController.text,
+                                  password: pwdInputController.text)
+                              .then((currentUser) => FirebaseFirestore.instance
+                                  .collection("users")
+                                  .doc(users.toString())
+                                  .set({
+                                    "id": users.toString(),
+                                    "fname": firstNameInputController.text,
+                                    "surname": lastNameInputController.text,
+                                    "email": emailInputController.text,
+                                  })
+                                  .then((result) => {
+                                        Navigator.pushAndRemoveUntil(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => MyHomePage(
+                                                      title:
+                                                          "Welcome"+firstNameInputController
+                                                                  .text,
+                                                              
+                                                      id: users.toString(),
+                                                    )),
+                                            (_) => false),
+                                        firstNameInputController.clear(),
+                                        lastNameInputController.clear(),
+                                        emailInputController.clear(),
+                                        pwdInputController.clear(),
+                                        confirmPwdInputController.clear()
+                                      })
+                                  .catchError((err) => print(err)))
+                              .catchError((err) => print(err));
+                        } else {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text("Error"),
+                                  content: Text("The passwords do not match"),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      child: Text("Close"),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    )
+                                  ],
+                                );
+                              });
+                        }
+                      }
+                    },
+                  ),
+                  Padding(padding:EdgeInsets.fromLTRB(20, 15, 10, 15)),
+                  Text("Already have an account?",),
+                  FlatButton(
+                    child: Text("Login here!"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              ),
+            ))));
+  }
+}
--- Rayie_Quizz/lib/login_screen.dart
+++ Rayie_Quizz/lib/login_screen.dart
@@ -1,5 +1,13 @@
+
+import 'package:QuizzTime/Register.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home.dart';
+
+
+
 
 TextStyle style = TextStyle(fontFamily: 'Aerial', fontSize: 20.0);
 
@@ -7,10 +15,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'Login',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple[300],
       ),
       home: MyLoginPage(title: 'Login'),
     );
@@ -25,112 +34,176 @@ class MyLoginPage extends StatefulWidget {
 }
 
 class _MyLoginPageState extends State<MyLoginPage> {
-  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  TextEditingController emailInputController;
+  TextEditingController pwdInputController;
+    TextStyle style = TextStyle(fontFamily: 'Aerial', fontSize: 20.0);
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  
+      @override
+    initState() {
+      emailInputController = new TextEditingController();
+      pwdInputController = new TextEditingController();
+      super.initState();
+    }
+  
+    String emailValidator(String value) {
+      Pattern pattern =
+          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+      RegExp regex = new RegExp(pattern);
+      if (!regex.hasMatch(value)) {
+        return 'Email format is invalid';
+      } else {
+        return null;
+      }
+    }
+  
+    String pwdValidator(String value) {
+      if (value.length < 8) {
+        return 'Password must be longer than 8 characters';
+      } else {
+        return null;
+      }
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    final emailField = TextField(
-      obscureText: false,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Email",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final passwordField = TextField(
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MyHomePage()),
-          );
-        },
-      ),
-    );
+//Forget Password
+    Future<void> resetPassword(String email) async {
+    await firebaseAuth.sendPasswordResetEmail(email: email);
+}
 
-    return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 155.0,
-                  child: Image.asset(
-                    "assets/logo.png",
-                    fit: BoxFit.contain,
+    @override
+    Widget build(BuildContext context) {
+      final emailField = TextFormField(
+        obscureText: false,
+        style: style,
+        key: _loginFormKey,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 15.0),
+            hintText: "Email:",
+            filled: true,
+            fillColor: Colors.white70,
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+                  controller: emailInputController,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: emailValidator,
+                      
+      );
+      final passwordField = TextFormField(
+        obscureText: true,
+        style: style,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 15.0),
+            hintText: "Password:",
+            filled: true,
+            fillColor: Colors.white70,
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+                controller: pwdInputController,
+                validator: pwdValidator,
+      );
+      final loginButon = Material(
+        elevation: 9.0,
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.lightBlue[300],
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          child: Text("Login",
+              textAlign: TextAlign.center,
+              style: style.copyWith(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
+       onPressed: () {
+                        if (_loginFormKey.currentState.validate()) {
+                          FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: emailInputController.text,
+                                  password: pwdInputController.text)
+                              .then((currentUser) => FirebaseFirestore.instance
+                                  .collection("users")
+                                  .doc(currentUser.toString())
+                                  .get()
+                                  .then((DocumentSnapshot result) =>
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => MyHomePage(
+                                                  ))))
+                                  .catchError((err) => print(err)))
+                              .catchError((err) => print(err));
+                        }
+       }
+        ),
+      );
+      final registerButton  = Material(
+        elevation: 9.0,
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.deepPurpleAccent,
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          child: Text("Register Now",
+              textAlign: TextAlign.center,
+              style: style.copyWith(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RegisterPage(),
+            ));
+          },
+        ),
+        
+      );
+  
+      return Scaffold(
+        resizeToAvoidBottomPadding: false, 
+        body: Center(
+          child: Container(
+            key: _loginFormKey,
+            color: Colors.blueAccent,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/logo.png"),
+                        fit: BoxFit.contain,
+                        )
+                    ),
                   ),
-                ),
-                SizedBox(height: 45.0),
-                emailField,
-                SizedBox(height: 25.0),
-                passwordField,
-                SizedBox(
-                  height: 35.0,
-                ),
-                loginButon,
-                SizedBox(
-                  height: 15.0,
-                ),
-              ],
+                  SizedBox(height: 45.0),
+                  emailField,
+                  SizedBox(height: 25.0),
+                  passwordField,
+                  SizedBox( height: 35.0),
+                  loginButon,
+                  SizedBox(height: 20.0),
+                  registerButton,
+                  Padding(padding:EdgeInsets.fromLTRB(20, 15, 10, 15)),
+                  Text("Forgot password?"),
+                  FlatButton(
+                    color: Colors.blue.shade400,
+                    child: Text("Reset"),
+                    onPressed: () {
+                      resetPassword(emailInputController.text);
+                    },
+                  )
+                  ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
+  
+ 
 
-final emailField = TextField(
-  style: style,
-  decoration: InputDecoration(
-      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-      hintText: "Email",
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-);
-
-final passwordField = TextField(
-  obscureText: true,
-  style: style,
-  decoration: InputDecoration(
-      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-      hintText: "Password",
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-);
-
-final loginButon = Material(
-  elevation: 5.0,
-  borderRadius: BorderRadius.circular(30.0),
-  color: Color(0xff01A0C7),
-  child: MaterialButton(
-    padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-    onPressed: () {},
-    child: Text("Login",
-        textAlign: TextAlign.center,
-        style:
-            style.copyWith(color: Colors.white, fontWeight: FontWeight.bold)),
-  ),
-);
